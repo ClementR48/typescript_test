f998ca66cb42a741a992e47bde767773cb19b92a
Clement <riera.clement@gmail.com>
2022-08-14 16:12:13 +0200

    installation package

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const title = document.querySelector("h1");
 
+title.addEventListener("click", handleClickTitle);
 
-const handleClickTitle = () => {
+function handleClickTitle(){
   setTimeout(() => {
     title.style.color = "#333"
   }, 1000)
   title.style.color = "red"
 }
-
-title.addEventListener("click", handleClickTitle);
-
-
